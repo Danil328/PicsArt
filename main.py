0e90f694b5f885fb72379890a63194ef5ae55a51
Danil Akhmetov <danil328@yandex.ru>
2018-11-15 21:35:40 +0500

    add predict

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import numpy as np
 from skimage.io import imread, imshow
+import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from model import Nest_Net
 from losses import  dice_coef_loss_bce, dice_coef, hard_dice_coef, binary_crossentropy
+from my_tools import rle_encoding, rle_decode
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import Adam
+from keras.models import load_model
 from keras.utils import Sequence
 
-path = 'PicsArt/data/'
-BATCH = 4
+# path = 'PicsArt/data/'
+path = '/media/danil/Data/Datasets/PicsArt/data/'
+BATCH = 12
 
 def load_train_data(path):
     print('===LOAD DATA===')
@@ -20,13 +24,36 @@ def load_train_data(path):
     train_mask_list = [imread(os.path.join(path, 'train_mask', img.split('.')[0]+'.png')) for img in train_images]
     return np.array(train_images_list), np.expand_dims(np.array(train_mask_list),-1)
 
+def load_test_data(path):
+    print('===LOAD TEST DATA===')
+    image_names = os.listdir(os.path.join(path, 'test'))
+    test_images_list = [imread(os.path.join(path, 'train', img)) for img in image_names]
+    return image_names, np.array(test_images_list)
+
+def make_predict(model):
+    print('===PREDICT===')
+    image_names, test_images_array = load_test_data(path)
+    test_images_array = test_images_array / 255.
+    predict_mask = model.predict(test_images_array, batch_size=1, verbose=1)
+    return test_images_array, predict_mask, image_names
+
+def create_submission(image_names, predicted_mask, threshold=0.5):
+    print('===CREATE SUBMISSION===')
+    predicted_mask[predicted_mask > threshold] = 1
+    predicted_mask[predicted_mask <= threshold] = 0
+    rle_mask = [rle_encoding(x) for x in predicted_mask]
+    sub = pd.DataFrame()
+    sub['image'] = image_names
+    sub['rle_mask'] = rle_mask
+    sub.to_csv('submission/submission.csv', index=False)
+
 def create_train_image_generator(X_train, y_train):
     data_gen_args = dict(featurewise_center=False,
                          featurewise_std_normalization=False,
                          rescale=1. / 255,
-                         rotation_range=0,
-                         width_shift_range=0.1,
-                         height_shift_range=0.1,
+                         rotation_range=10,
+                         width_shift_range=0.15,
+                         height_shift_range=0.15,
                          zoom_range=0.2,
                          horizontal_flip=True)
     image_datagen = ImageDataGenerator(**data_gen_args)
@@ -40,7 +67,7 @@ def create_train_image_generator(X_train, y_train):
     return train_generator
 
 def create_callbaks():
-    checkpoint = ModelCheckpoint('PicsArt/weights/unet++.h5', monitor='val_dice_coef', mode='max', save_best_only=True)
+    checkpoint = ModelCheckpoint('weights/unet++.h5', monitor='val_dice_coef', mode='max', save_best_only=True, verbose=1)
     return [checkpoint]
 
 if __name__ == '__main__':
@@ -51,13 +78,15 @@ if __name__ == '__main__':
 
     train_generator = create_train_image_generator(X_train, y_train)
     model = Nest_Net(320, 240, 3)
-    model.compile(optimizer=Adam(1e-4, decay=1e-5), loss=dice_coef_loss_bce, metrics=[dice_coef, hard_dice_coef, binary_crossentropy])
-    callbaacks = create_callbaks()
-
+    model.compile(optimizer=Adam(1e-4, decay=1e-6), loss=dice_coef_loss_bce, metrics=[dice_coef, hard_dice_coef, binary_crossentropy])
+    callbacks = create_callbaks()
+    
+    print('===FIT MODEL===')
     model.fit_generator(train_generator,
                         steps_per_epoch = X_train.shape[0]/BATCH,
                         epochs=20,
-                        callbacks=callbaacks,
+                        verbose=1,
+                        callbacks=callbacks,
                         validation_data=(X_val, y_val))
 
     # x,y = next(train_generator)
@@ -68,3 +97,13 @@ if __name__ == '__main__':
     # imshow(y[0,...,0])
     # plt.show()
 
+    model = load_model('weights/unet++.h5', compile = False)
+    test_image_array, predicted_mask, test_image_names = make_predict(model)
+    create_submission(test_image_names, predicted_mask)
+
+    # plt.figure()
+    # imshow(test_image_array[0])
+    # plt.show()
+    # plt.figure()
+    # imshow(predicted_mask[0,...,0])
+    # plt.show()
--- model.py
+++ model.py
@@ -18,31 +18,6 @@ dropout_rate = 0.5
 act = "relu"
 
 
-def mean_iou(y_true, y_pred):
-    prec = []
-    for t in np.arange(0.5, 1.0, 0.05):
-        y_pred_ = tf.to_int32(y_pred > t)
-        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
-        K.get_session().run(tf.local_variables_initializer())
-        with tf.control_dependencies([up_opt]):
-            score = tf.identity(score)
-        prec.append(score)
-    return K.mean(K.stack(prec), axis=0)
-
-
-# Custom loss function
-def dice_coef(y_true, y_pred):
-    smooth = 1.
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-
-
-def bce_dice_loss(y_true, y_pred):
-    return 0.5 * keras.losses.binary_crossentropy(y_true, y_pred) - dice_coef(y_true, y_pred)
-
-
 ########################################
 # 2D Standard
 ########################################
