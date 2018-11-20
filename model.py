15348316112cc005bf1914825f87c138dac612bf
Danil <Danil328@yandex.ru>
2018-11-20 11:47:07 +0500

    Update model.py

--- model.py
+++ model.py
@@ -73,15 +73,23 @@ def scSE_block(input_tensor):
 #     x = Activation(act)(x)
 #     return x
 
-def standard_unit(input_tensor, stage, nb_filter, kernel_size=3):
-    input = Conv2D(nb_filter, (kernel_size, kernel_size), activation=act, name='conv'+stage+'_0', kernel_initializer = 'he_normal', padding='same', kernel_regularizer=l2(1e-4))(input_tensor)
-    input = BatchNormalization()(input)
+# def standard_unit(input_tensor, stage, nb_filter, kernel_size=3):
+#     input = Conv2D(nb_filter, (kernel_size, kernel_size), activation=act, name='conv'+stage+'_0', kernel_initializer = 'he_normal', padding='same', kernel_regularizer=l2(1e-4))(input_tensor)
+#     input = BatchNormalization()(input)
     
+#     x = Conv2D(nb_filter, (kernel_size, kernel_size), activation=act, name='conv' + stage + '_1', kernel_initializer='he_normal', padding='same', kernel_regularizer=l2(1e-4))(input_tensor)
+#     x = BatchNormalization()(x)
+#     x = Conv2D(nb_filter, (kernel_size, kernel_size), activation=act, name='conv'+stage+'_2', kernel_initializer = 'he_normal', padding='same', kernel_regularizer=l2(1e-4))(x)
+#     x = BatchNormalization()(x)
+#     x = Add()([x, input])
+#     return x
+
+def standard_unit(input_tensor, stage, nb_filter, kernel_size=3): 
     x = Conv2D(nb_filter, (kernel_size, kernel_size), activation=act, name='conv' + stage + '_1', kernel_initializer='he_normal', padding='same', kernel_regularizer=l2(1e-4))(input_tensor)
     x = BatchNormalization()(x)
     x = Conv2D(nb_filter, (kernel_size, kernel_size), activation=act, name='conv'+stage+'_2', kernel_initializer = 'he_normal', padding='same', kernel_regularizer=l2(1e-4))(x)
-    x = BatchNormalization()(x)
-    x = Add()([x, input])
+    conv_d1b_c = scSE_block(conv_d1b_c)
+    x = BatchNormalization()(conv_d1b_c)
     return x
 
 """
